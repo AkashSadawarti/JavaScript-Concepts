99f38e59d5a54af75fa7a9df184cfaf2fcdc8c2c
Akash Sadawarti <sadawarti.akash@gmail.com>
2022-11-15 22:03:40 +0530

    Console User Input and Output

---
+++ JSProgram3ConsoleInputOutput/JSProgram3ConsoleInputOutput.js
@@ -0,0 +1,10 @@
+
+//for Output on console Window
+
+console.log('Hello Akash, Welcome to JavaScript Learning!')
+
+//For User input on Console window
+let showMessage = prompt('Hi.. Enter your Name')
+console.log(showMessage)
+console.log(`Welcome to JavaScript Learning ${showMessage}.`)
+
