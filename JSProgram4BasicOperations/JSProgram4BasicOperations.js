dabaebe77a6037ddde45044f637223b18bac8d8a
Akash Sadawarti <sadawarti.akash@gmail.com>
2022-11-15 22:05:22 +0530

    Basic operations Add,Sub,Mult,Div,Comparison

---
+++ JSProgram4BasicOperations/JSProgram4BasicOperations.js
@@ -0,0 +1,27 @@
+// Basic Operatons Using declaring 
+let a = 10;
+let b = 20;
+console.log('let A = 10 and B = 20 are two Numbers')
+console.log('Addition of two Numbers is ' + (a+b))
+console.log('Substraction of two Numbers is ' + (a-b))
+console.log('Multiplication of two Numbers is ' + (a*b))
+console.log('Division of two Numbers is ' + (a/b))
+console.log('Number A is less than B = ' + (a<b))
+console.log('Number B is greater than A = ' + (b>a))
+console.log('Number A is greater than B = ' + (a>b))
+console.log('Number B is Less than A = ' + (b<a))
+
+//Basic Operations by User Input
+const p = prompt('Enter the First Number')
+const q = prompt('Enter the Second Number')
+// `${}` can be used to enter user value it changes dynamically according to user  
+console.log(`let First Number = ${p} \n   Second Number = ${q} are two Numbers `)
+console.log(`Addition of two Numbers is ${p+q}`)
+console.log('Substraction of two Numbers is ' + (p-q))
+console.log('Multiplication of two Numbers is ' + (p*q))
+console.log('Division of two Numbers is ' + (p/q))
+console.log('Number P is less than Q = ' + (p<q))
+console.log('Number Q is greater than P = ' + (q>p))
+console.log('Number P is greater than Q = ' + (p>q))
+console.log('Number Q is Less than P = ' + (q<p))
+
