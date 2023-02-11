6ed765abbf778661f1a7548763f79f37972e6a6a
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-02-11 21:11:52 +0530

    Concept of Array

---
+++ JSProgram8Arrays/JSProgram8Arrays.js
@@ -0,0 +1,23 @@
+
+// Concept of Array
+let cars = ["Alto","Figo","Verna"]
+
+console.log(cars)
+
+// push() method insert Element in last position fast as compare to shift and unshift
+// console.log(cars.push("Creta"));
+// console.log(cars)
+
+// pop() method remove Element in last position fast as compare to shift and unshift
+// console.log(cars.pop())
+// console.log(cars)
+
+// shift() method insert Element in first position  but slow in speed
+
+console.log(cars.shift())
+console.log(cars)
+
+// unshift() method removes Element in first position but slow in speed
+
+console.log(cars.unshift('Grant Vitara'))
+console.log(cars)
