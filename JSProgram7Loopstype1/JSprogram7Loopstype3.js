a1013f8978464f7897ebb16789f7262e799cd68c
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-02-11 21:13:12 +0530

    Loops and types

---
+++ JSProgram7Loopstype1/JSprogram7Loopstype3.js
@@ -0,0 +1,61 @@
+"use strict";  //keyword is definition specific
+
+// Concept of While loop
+// EX: 1
+// let i = 0;
+// while (i <= 10) {
+//     console.log(i)
+// i++
+// }
+
+// EX: 2
+// let array = [1,4,5,8,7,9,4,6,5,6,4]
+// let i = array[0]
+// while(i <= array.length-1){
+//    console.log(array[i]);
+//     i++
+// }
+
+// EX: 3
+// let i = 5;
+// while(i >= 1){
+//     console.log(i)
+//    i--
+// }
+
+
+// // EX: 4
+// let x = 10;
+// let i = 0
+// let total = 0;
+// while (i <= x) {
+//     total = total + i;
+//     i++
+// }
+// console.log(total)
+// let total2 = 0;
+// console.log(total2 = x * (x + 1) / 2)  // run fast
+
+
+//Concept of for loop
+
+// for (let i = 0; i <= 10; i++) {     //print 1 to 10
+//     console.log(i);
+// }
+
+// for (let x = 0; x <= 10; x++) {
+//     if (x == 4) {
+//         break;      //breaks from printing after 3
+//     }
+//     console.log(x)
+// }
+
+// for (let x = 0; x <= 10; x++) {
+//     if (x == 4) {
+//         continue;  //skips 4 from 1 to 10
+//     }
+//     console.log(x)
+// }
+
+
+  
