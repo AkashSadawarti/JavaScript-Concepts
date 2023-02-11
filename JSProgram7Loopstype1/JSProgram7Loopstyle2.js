d14d557ae279619d10a6817c0a64869cf7baaa28
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-02-11 21:13:26 +0530

    Loops and types

---
+++ JSProgram7Loopstype1/JSProgram7Loopstyle2.js
@@ -0,0 +1,41 @@
+//switch loop 
+
+// // syntax : 
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// Example weekdays
+let day = -1
+switch (day) {
+    case 0:
+         console.log("sunday");
+        break;
+    case 1:
+         console.log("monday");
+        break;
+    case 2:
+         console.log("tuesday");
+        break;
+    case 3:
+         console.log("wednesday");
+        break;
+    case 4:
+         console.log("thursday");
+        break;
+    case 5:
+         console.log("friday");
+        break;
+    case 6:
+         console.log("saturday");
+        break;
+
+    default:
+        console.log("invalid day")
+        break;
+}
