c45100d433a69842ee0bd5dcc1728cc25068e8f0
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-03-11 09:02:04 +0530

    objects update

--- JSProgram10Objects/loops-in-objects.js
+++ JSProgram10Objects/loops-in-objects.js
@@ -7,12 +7,37 @@ let person = {
 }
 
 //for in loop
-for (let key in person) {
-    console.log(key)  //gives only keys as result
+// for (let key in person) {
+//     console.log(key)  //gives only keys as result
+// }
+// for (let key in person) {
+//     console.log(person[key])  //gives only values as result
+// }
+// for (let key in person) {
+//     console.log(`${key} : ${person[key]}`) //gives values and keys as result
+// }
+
+//for of loop
+for(let keys of Object.keys(person)){
+    console.log(keys,":",person[keys])
 }
-for (let key in person) {
-    console.log(person[key])  //gives only values as result
+
+//Computed properties
+const key1 = 'Object1'
+const key2 = 'Object2'
+
+const value1 = 'myvalue1'
+const value2 = 'myvalue1'
+
+// 1st way
+let Obj = {
+    [key1] : value1,
+    [key2] : value2
 }
-for (let key in person) {
-    console.log(`${key} : ${person[key]}`) //gives values and keys as result
-}
+console.log(Obj)
+// 2nd way
+
+let obj ={}
+obj[key1] = value1
+obj[key2] = value2
+console.log(obj)
