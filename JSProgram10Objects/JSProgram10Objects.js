2636c789f31cb1c76e8968030b2c08448b3b1f71
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-03-14 22:06:20 +0530

    Object update

--- JSProgram10Objects/JSProgram10Objects.js
+++ JSProgram10Objects/JSProgram10Objects.js
@@ -42,32 +42,32 @@ console.log(obj2);
 //Objects concat with Spread operators
 let obj3 = {
   key1: "value1",
-  key2: "value2"
+  key2: "value2",
 };
 
 let obj4 = {
   key3: "value1",
-  key4: "value2"
+  key4: "value2",
 };
 
-let newObj = {...obj3,...obj4, key46 : "value42"}
-console.log(newObj)
+let newObj = { ...obj3, ...obj4, key46: "value42" };
+console.log(newObj);
 
-let Obj5 = {...["value1","value2"]}
-let Obj6 = {..."56484"}
+let Obj5 = { ...["value1", "value2"] };
+let Obj6 = { ..."56484" };
 console.log(Obj5);
 console.log(Obj6);
 
 let Obj7 = {
-  band : "Micheal Jackson",
-  song : "Waving flag",
-  song2 : "Waka Waka",
-  year : 1975
-}
-//value can be accessed by simple creating objects 
+  band: "Micheal Jackson",
+  song: "Waving flag",
+  song2: "Waka Waka",
+  year: 1975,
+};
+//value can be accessed by simple creating objects
 let b1 = Obj7.band;
 let s1 = Obj7.song;
-console.log(b1,s1);
+console.log(b1, s1);
 
 //destructuring Objects
 // let {band ,song , song2,year } = Obj7; //write only keys in {}
@@ -75,23 +75,33 @@ console.log(b1,s1);
 // console.log(song);
 // console.log(band,song,song2,year)
 
-let {band ,song , ...restprop } = Obj7; // use of spread operator for remaining properties
-console.log(restprop)
+let { band, song, ...restprop } = Obj7; // use of spread operator for remaining properties
+console.log(restprop);
 
 //Objects with muliple data
 let Obj8 = [
   {
-    userid : 1,
-    username : 'Akash'
+    userid: 1,
+    username: "Akash",
   },
   {
-    userid : 2,
-    username : 'Alex'
+    userid: 2,
+    username: "Alex",
   },
   {
-    userid : 3,
-    username : 'Adi'
-  }
-]
+    userid: 3,
+    username: "Adi",
+  },
+];
 
 console.log(Obj8);
+// destructure object Array``
+const [user1,user2,user3] = Obj8
+console.log(user1)
+console.log(user2)
+
+// const [user1,user2,...rest] = Obj8; with spread operator
+// console.log(rest)
+  
+const [{userid : idofFirstobject}, , {username: usernameof3rdobject}] = Obj8  //space is for 2nd object in array
+console.log(idofFirstobject,usernameof3rdobject);
