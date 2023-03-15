c1cffd7c8ea0186cc7f4a7e45b43076d130cb9a4
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-03-15 07:36:39 +0530

    functions concept

---
+++ js-program11-functions/js-program11-functions.js
@@ -0,0 +1,11 @@
+//Functions : it has some purpose to do eg a particular work/action is donne by a function.
+
+// eg 1: for defining function and syntax
+
+function playSong() {
+  console.log("Happy Birthday to you...... "); 
+}
+//this action can ne performed n number of times by calling function
+playSong();
+
+//
