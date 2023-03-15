9894b07d9e40757228ea19b5aafe0d242400fd4e
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-03-15 08:31:26 +0530

    functions concept

--- js-program11-functions/js-program11-functions.js
+++ js-program11-functions/js-program11-functions.js
@@ -3,17 +3,18 @@
 //Functions : it has some purpose to do eg a particular work/action is donne by a function.
 // eg 1: for defining function and syntax
 
+//Function declaration
 function playSong() {
   console.log("Happy Birthday to you...... ");
 }
 //this action can ne performed n number of times by calling function
-// playSong();
+playSong();
 
 //eg 2
 function Add(number1, number2) {
   console.log(number1 + number2);
 }
-// Add(5,5) //--> Values which are passed are called arguments
+Add(5,5) //--> Values which are passed are called arguments
 
 // eg 3 calling function in another function
 
@@ -30,7 +31,7 @@ function evenOdd(number) {
     return true;
   }
   return false;
-  // return number % 2 === 0;  //using sing expression
+  // return number % 2 === 0;  //using single expression
   // return (number % 2 === 0) ? true : false ; //using ternary operator
 }
 console.log(evenOdd(7));
@@ -53,3 +54,11 @@ function Target(Arr, targ) {
   return console.log("Target Not Found"); 
 }
 Target([0, 1, 3, 4, 6, 8, 5], 15);
+
+//function expression:
+// 2nd way of declaring function by giving it like variable this type also called function expression    
+
+let user = function(){
+    console.log('Hello User')
+}
+console.log(user());
