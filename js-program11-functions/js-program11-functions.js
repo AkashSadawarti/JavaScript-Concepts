38fb0b683cf30f89c55d7d3001c5de941fde3339
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-03-15 08:00:35 +0530

    functions concept update

--- js-program11-functions/js-program11-functions.js
+++ js-program11-functions/js-program11-functions.js
@@ -4,20 +4,40 @@
 // eg 1: for defining function and syntax
 
 function playSong() {
-  console.log("Happy Birthday to you...... "); 
+  console.log("Happy Birthday to you...... ");
 }
 //this action can ne performed n number of times by calling function
 // playSong();
 
-//eg 2 
-function Add(number1,number2){
-  console.log(number1+number2);
+//eg 2
+function Add(number1, number2) {
+  console.log(number1 + number2);
 }
 // Add(5,5) //--> Values which are passed are called arguments
 
 // eg 3 calling function in another function
 
-function callAnotherFunction(){
-  return  playSong(),Add(10,15);
+function callAnotherFunction() {
+  return playSong(), Add(10, 15);
 }
 console.log(callAnotherFunction());
+
+//WAP for even odd in functions return true/false
+
+function evenOdd(number) {
+  if (number % 2 === 0) {  //using if else condition
+    return true;
+  }
+  return false;
+// return number % 2 === 0;  //using sing expression
+// return (number % 2 === 0) ? true : false ; //using ternary operator
+
+}
+console.log(evenOdd(7));
+
+//WAP print first character of string
+function firstChar(str){
+    let res = str[0];
+    return res;
+}
+console.log(firstChar("Char"));
