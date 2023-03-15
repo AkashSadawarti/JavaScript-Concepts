b0d42ccbc82b293a6e9ee4da6f5282563eeb9e7d
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-03-15 07:46:22 +0530

    functions concept

--- js-program11-functions/js-program11-functions.js
+++ js-program11-functions/js-program11-functions.js
@@ -7,6 +7,17 @@ function playSong() {
   console.log("Happy Birthday to you...... "); 
 }
 //this action can ne performed n number of times by calling function
-playSong();
+// playSong();
 
-//
+//eg 2 
+function Add(number1,number2){
+  console.log(number1+number2);
+}
+// Add(5,5) //--> Values which are passed are called arguments
+
+// eg 3 calling function in another function
+
+function callAnotherFunction(){
+  return  playSong(),Add(10,15);
+}
+console.log(callAnotherFunction());
