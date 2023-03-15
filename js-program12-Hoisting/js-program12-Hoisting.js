ff18f385a4e1864335c2aa2a239d394455268446
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-03-15 09:08:25 +0530

    Hoisting update

---
+++ js-program12-Hoisting/js-program12-Hoisting.js
@@ -0,0 +1,21 @@
+// Hoisting 
+
+// let ex
+
+// function declaration 
+greet();  //o/p = Hello world , here it will show o/p this called hoisting
+      
+function  greet(){
+    console.log('Hello World!');
+}
+
+// now if we try to call this function written in arrow function and function expression it will not Work.
+
+// for eg
+// greet2();  //errror : cannot access greet2 before initialization
+// let greet2 = function() {
+//    console.log("good Morning");
+// }
+
+greet3(); //TypeError: undefined is not a function
+let greet3 = () => console.log("good Evening");
