4d944e8a610ef69837820c2d6ad2b80369960328
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-03-17 08:20:28 +0530

    function in functions update

--- js-program11-functions/js-program11-functions-inside-functions.js
+++ js-program11-functions/js-program11-functions-inside-functions.js
@@ -11,3 +11,30 @@
 
 //function in functions eg=>
 
+function movie(){
+    let scene = () =>{
+        console.log('Aircraft Fighting');
+    }
+
+    let soldier = (ladies ,mens ) => {
+        console.log(`Total Soldier Strength = ${ladies + mens}`)
+    }
+
+    console.log("movie name : Lakshya")
+    soldier(5000,1000);   //call this function in function so that it will be accessible soldier() will get printed not scene()
+}
+movie()
+
+//lexical scope
+
+function myApp(){
+    let myVar = "value8";
+
+    function insideFunc(){
+        // let myVar = "value67";
+        console.log(`inside function : ${myVar}`);
+    }
+    insideFunc()  //this function will prefer myvar value from its own function but when same variable is defined in same env but not in function it will take myvar value from it parent function i.e env.
+}
+myApp() //1) o/p = inside function : value67
+        //2) comment myvar from local scope o/p = inside function : value8
