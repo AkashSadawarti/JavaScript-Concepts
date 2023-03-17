2d70fc15716c8ef56736e8cbbb08470ad27cc5be
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-03-17 08:00:14 +0530

    function in functions

---
+++ js-program11-functions/js-program11-functions-inside-functions.js
@@ -0,0 +1,13 @@
+ //Function calling function
+//  function greet(){
+//     console.log("Hello student");
+// }
+
+// function student(){
+//     greet();
+//     console.log(`Akash`);
+// }
+// student()
+
+//function in functions eg=>
+
