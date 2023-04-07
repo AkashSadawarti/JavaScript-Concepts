333dc55de8e9ad1f9a038aceb271e53fafa83c5a
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-07 16:51:02 +0530

    Patterns

---
+++ js-program-patterns/js-program-patterns.js
@@ -0,0 +1,11 @@
+// Square pattern
+// let row = 5
+// let string = ''
+// for(let i=0;i<row;i++){
+//   for(let j=0;j<row;j++){
+//     string += '*'
+//   }
+//   string += "\n"
+// }
+// console.log(string);
+
