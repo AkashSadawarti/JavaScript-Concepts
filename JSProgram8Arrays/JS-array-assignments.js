437a15658d0196f35dadfe5f8b777d8795b21e8d
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-11 16:47:18 +0530

    array assinment update

--- JSProgram8Arrays/JS-array-assignments.js
+++ JSProgram8Arrays/JS-array-assignments.js
@@ -8,3 +8,9 @@ for (let i = 0; i < Array.length; i++) {
   }
 }
 console.log(uniqueElements);
+let sum = 0
+for(let i = 0; i<= uniqueElements.length;i++)
+{
+  sum = uniqueElements.reduce((a,b) => a+b);
+}
+console.log(sum);
