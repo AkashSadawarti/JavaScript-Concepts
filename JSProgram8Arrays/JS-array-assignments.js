e59b65a868a27554634de58eb3d5f98a8f5dbbe8
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-11 16:46:14 +0530

    array assignments

--- JSProgram8Arrays/JS-array-assignments.js
+++ JSProgram8Arrays/JS-array-assignments.js
@@ -1,2 +1,10 @@
 // sum of Elements in Array without duplicates
 
+
+let uniqueElements = []
+for (let i = 0; i < Array.length; i++) {
+  if (uniqueElements.indexOf(Array[i]) === -1) {
+    uniqueElements.push(Array[i])
+  }
+}
+console.log(uniqueElements);
