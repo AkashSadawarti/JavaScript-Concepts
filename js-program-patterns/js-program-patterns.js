38a1533b8f925a92b99b90f3c6b4dd7e4d8d5ad4
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-11 17:29:35 +0530

    pattern update

--- js-program-patterns/js-program-patterns.js
+++ js-program-patterns/js-program-patterns.js
@@ -30,7 +30,7 @@
 //     string3 += "\n"
 //   }
 //   console.log(string3);
-  
+
 //   // triangle right
 //   let row4 = 5
 //   let string4 = ""
@@ -45,3 +45,56 @@
 //     }
 //     console.log(string4);
 
+//     let row5 = 5
+//     let string5 = ""
+//     for(let i=1; i<=row4; i++){
+//         for(let j=1 ;j<=row4-i; j++){
+//           string5 += " ";
+//         }
+//         for(let k=0; k<2*i-1;k++){
+//            string5 += "*";
+//         }
+//         string5 += "\n"
+//       }
+//   console.log(string5);
+
+let row6 = 6
+let string6 = ""
+for(let i=0; i<=row6; i++){
+    for(let j=1 ;j<=i; j++){
+      string6 += " ";
+    }
+    for(let k=0; k<2*(row6-i);k++){
+       string6 += "*";
+    }
+    string6 += "\n"
+  }
+  console.log(string6);
+
+
+// let row7 = 4;
+// let string7 = "";
+
+//     for (let i = 0; i < row7; i++) {
+//         for (let j = 0; j <= 1; j++) {
+//             string7 += " ";
+//         }
+//         for (let k = 0; k <= 2*(row7-i)-1 ; k++) {
+//             string7 += "*";
+//         }
+//         string7 += "\n";
+//     }
+//     for(let i=1;i<row7;i++){
+//         for(let j=i;j<row7;j++){
+//             string7 += " ";
+//         }
+//         for(let k=0;k<=2*(row7-i); k++){
+//             string7 += "*"
+//         }
+//         string7 += "\n"
+//     };
+
+// console.log(string7);
+
+// 'use strict'
+// let arr  = [1]
