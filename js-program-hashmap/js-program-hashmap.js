44653437203d8a8ae4706ce1e947e2aecde97884
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-12 14:35:00 +0530

    map update

--- js-program-hashmap/js-program-hashmap.js
+++ js-program-hashmap/js-program-hashmap.js
@@ -2,5 +2,10 @@
 // HashMap is used to any amount of data  in key and value pair
 
 let map = new Map()   //map {}
-console.log(map);
+// console.log(map);
+
+map.set(1,100)      //for setting up key and value
+map.set("a",1000)
+map.set(9,9.020)
+
 
