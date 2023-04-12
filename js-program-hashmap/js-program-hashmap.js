0ab6e96b1a8e51b30a0964305419b7d9f3bc768b
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-12 14:57:43 +0530

    map update

--- js-program-hashmap/js-program-hashmap.js
+++ js-program-hashmap/js-program-hashmap.js
@@ -9,6 +9,7 @@ map.set("a",1000)
 map.set(9,9.020)
 
 
+// for taking o/p
 console.log(map);   //Map(3) { 1 => 100, 'a' => 1000, 9 => 9.02 }
 console.log(map.size);  //3
 console.log(map.get(1)); //100
