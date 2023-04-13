f70485a89cb3878b787370c258d1ae0fc7d7eec9
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-13 15:53:45 +0530

    leet code

--- leet-code/leet-code-977.js
+++ leet-code/leet-code-977.js
@@ -4,4 +4,13 @@ let Arr = [-1, 0, 1, 8, 4, 3]
 for (let i = 0; i < Arr.length; i++) {
     sortedArray.push(Arr[i] * Arr[i]);
 }
-
+let temp;
+for (let i = 0; i < sortedArray.length - 1; i++) {
+    for (let j = i + 1; j < sortedArray.length; j++)
+        if (sortedArray[i] > sortedArray[j]) {
+            temp = sortedArray[i]
+            sortedArray[i] = sortedArray[i + 1]
+            sortedArray[i + 1] = temp
+        }
+}
+console.log(sortedArray);
