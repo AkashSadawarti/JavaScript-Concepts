4e42d2e64fd750bf97e9213a3663800c064c7e20
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-17 13:30:30 +0530

    leet code update

--- leet-code/leet-code-628.js
+++ leet-code/leet-code-628.js
@@ -1,3 +1,14 @@
 // 628. Maximum Product of Three Numbers
 // product of element in Array
 let nums = [7,3,1,0,0,6]
+
+let temp2 ;
+for(let i=0;i<nums.length-1;i++){
+    for(let j=i+1;j<nums.length;j++)
+   if(nums[i] > nums[j]){
+       temp2 = nums[i]
+       nums[i] = nums[i+1]
+       nums[i+1] = temp2
+   }
+}
+
