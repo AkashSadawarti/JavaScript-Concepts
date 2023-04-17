0e50d461064531bcead4431af4741e3e16381681
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-17 13:34:45 +0530

    leet code update

--- leet-code/leet-code-628.js
+++ leet-code/leet-code-628.js
@@ -11,4 +11,7 @@ for(let i=0;i<nums.length-1;i++){
        nums[i+1] = temp2
    }
 }
+let Res = nums[nums.length-1]  * nums[nums.length-2] * nums[nums.length-3] 
+let Res2 = nums[0] * nums[1] * nums[nums.length -1]
+console.log(Math.max(Res,Res2));
 
