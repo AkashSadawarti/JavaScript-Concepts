2f47c42f408ab27e83ea394af201463e16b39120
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-18 15:41:19 +0530

    leet code update

--- leet-code/leet-code-1662.js
+++ leet-code/leet-code-1662.js
@@ -7,3 +7,9 @@
  
 
 // Example 1:
+// Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
+// Output: true
+// Explanation:
+// word1 represents string "ab" + "c" -> "abc"
+// word2 represents string "a" + "bc" -> "abc"
+// The strings are the same, so return true.
