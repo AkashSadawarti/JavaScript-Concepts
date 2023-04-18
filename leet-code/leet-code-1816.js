99c02adeae0540b5a5061cff13cc6009c3de0476
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-18 13:41:21 +0530

    leet code update

--- leet-code/leet-code-1816.js
+++ leet-code/leet-code-1816.js
@@ -7,3 +7,12 @@
 
 // For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
 // You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it contains only the first k​​​​​​ words. Return s​​​​​​ after truncating it
+
+// Example 1:
+
+// Input: s = "Hello how are you Contestant", k = 4
+// Output: "Hello how are you"
+// Explanation:
+// The words in s are ["Hello", "how" "are", "you", "Contestant"].
+// The first 4 words are ["Hello", "how", "are", "you"].
+// Hence, you should return "Hello how are you".
