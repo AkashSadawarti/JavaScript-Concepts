525ae94b634ed88c1e283c4d411c392d3ed9cfdb
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-20 11:28:36 +0530

    leetcode update

--- leet-code/leet-code-1512.js
+++ leet-code/leet-code-1512.js
@@ -3,6 +3,11 @@
 // Given an array of integers nums, return the number of good pairs.
 
 // A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+// Example 1:
+
+// Input: nums = [1,2,3,1,1,3]
+// Output: 4
+// Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
 let nums = nums = [1,2,3,1,1,3]
 var numIdenticalPairs = function(nums) {
     let pairsCount = 0
