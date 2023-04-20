958fd6cded570743c16214bb0fd4a69cd84544bc
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-20 11:25:43 +0530

    leetcode update

---
+++ leet-code/leet-code-1365.js
@@ -0,0 +1,11 @@
+// 1365. How Many Numbers Are Smaller Than the Current Number
+// Input: nums = [8,1,2,2,3]
+// Output: [4,0,1,1,3]
+// Explanation: 
+// For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3). 
+// For nums[1]=1 does not exist any smaller number than it.
+// For nums[2]=2 there exist one smaller number than it (1). 
+// For nums[3]=2 there exist one smaller number than it (1). 
+// For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+
+
