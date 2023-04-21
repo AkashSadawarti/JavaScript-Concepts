ef5a13eaea575815f9efb845ff4da7b4481fdfe4
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-21 10:55:21 +0530

    leet code update

---
+++ leet-code/leet-code-2185.js
@@ -0,0 +1,11 @@
+// 2185. Counting Words With a Given Prefix
+
+// Companies
+// You are given an array of strings words and a string pref.
+
+// Return the number of strings in words that contain pref as a prefix.
+
+// A prefix of a string s is any leading contiguous substring of s.
+
+ 
+
