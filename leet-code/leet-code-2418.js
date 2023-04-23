1f1a9b65433c091bd5a16fbfbbac52f9680389f3
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-23 14:29:01 +0530

    leet code

--- leet-code/leet-code-2418.js
+++ leet-code/leet-code-2418.js
@@ -3,27 +3,3 @@
 // For each index i, names[i] and heights[i] denote the name and height of the ith person.
 
 // Return names sorted in descending order by the people's heights.
-// Example 1:
-
-// Input: names = ["Mary","John","Emma"], heights = [180,165,170]
-// Output: ["Mary","Emma","John"]
-// Explanation: Mary is the tallest, followed by Emma and John.
-let temp1
-let temp2
-let names = ["Mary","John","Emma"], heights = [180,165,170]
- for(let i=0;i< heights.length;i++){
-    for(let j=0;j< heights.length;j++){
-        if(heights[i] > heights[j]){
-             temp = heights[i]
-             heights[i] = heights[j];
-             heights[j] = temp;
-
-             temp2 = names[i]
-             names[i] = names[j];
-             names[j] = temp2;
-        }
-    }
-
-}
-console.log(names);
-console.log(heights);
