7672f72cca1d622a12607575745f9ffd25ed5baa
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-23 14:26:27 +0530

    leet code

---
+++ leet-code/leet-code-2011.js
@@ -0,0 +1,15 @@
+// 2011. Final Value of Variable After Performing Operations
+// Easy
+// 1.2K
+// 152
+// Companies
+// There is a programming language with only four operations and one variable X:
+
+// ++X and X++ increments the value of the variable X by 1.
+// --X and X-- decrements the value of the variable X by 1.
+// Initially, the value of X is 0.
+
+// Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+
+ 
+
