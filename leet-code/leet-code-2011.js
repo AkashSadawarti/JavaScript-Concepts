f402152f8b016646f7119a36c35b6fdb0dacbb88
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-23 14:27:01 +0530

    leet code update

--- leet-code/leet-code-2011.js
+++ leet-code/leet-code-2011.js
@@ -13,3 +13,13 @@
 
  
 
+// Example 1:
+
+// Input: operations = ["--X","X++","X++"]
+// Output: 1
+// Explanation: The operations are performed as follows:
+// Initially, X = 0.
+// --X: X is decremented by 1, X =  0 - 1 = -1.
+// X++: X is incremented by 1, X = -1 + 1 =  0.
+// X++: X is incremented by 1, X =  0 + 1 =  1.
+
