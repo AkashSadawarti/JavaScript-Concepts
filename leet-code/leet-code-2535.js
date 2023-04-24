b61b4b1d96bf274ea995f77d62fc6feeafcac401
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-24 16:27:00 +0530

    leet code update

---
+++ leet-code/leet-code-2535.js
@@ -0,0 +1,12 @@
+// 2535. Difference Between Element Sum and Digit Sum of an Array
+
+// You are given a positive integer array nums.
+
+// The element sum is the sum of all the elements in nums.
+// The digit sum is the sum of all the digits (not necessarily distinct) that appear in nums.
+// Return the absolute difference between the element sum and digit sum of nums.
+
+// Note that the absolute difference between two integers x and y is defined as |x - y|.
+
+
+
