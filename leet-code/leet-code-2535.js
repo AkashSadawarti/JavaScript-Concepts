53e5582c06b806cf3ab3457a51fc53a6b3600646
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-24 16:27:53 +0530

    leetcode update

--- leet-code/leet-code-2535.js
+++ leet-code/leet-code-2535.js
@@ -10,3 +10,12 @@
 
 
 
+// Example 1:
+
+// Input: nums = [1,15,6,3]
+// Output: 9
+// Explanation: 
+// The element sum of nums is 1 + 15 + 6 + 3 = 25.
+// The digit sum of nums is 1 + 1 + 5 + 6 + 3 = 16.
+// The absolute difference between the element sum and digit sum is |25 - 16| = 9.
+
