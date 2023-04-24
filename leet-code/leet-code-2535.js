b3b961f2c919fb24cbcb1fa882e8cb74b994fa3e
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-24 16:29:08 +0530

    leet code update

--- leet-code/leet-code-2535.js
+++ leet-code/leet-code-2535.js
@@ -19,3 +19,35 @@
 // The digit sum of nums is 1 + 1 + 5 + 6 + 3 = 16.
 // The absolute difference between the element sum and digit sum is |25 - 16| = 9.
 
+var differenceOfSum = function (nums) {
+    let sumOfElements = 0
+    for (let i = 0; i < nums.length; i++) {
+        sumOfElements = sumOfElements + nums[i]
+    }
+    console.log(sumOfElements);
+    let sumOfDigits = 0;
+    let rem;
+    for (let i = 0; i < nums.length; i++) {
+        if ( nums[i] > 9) {
+            while (nums[i] != 0) {
+                rem = nums[i] % 10;
+                sumOfDigits += rem;
+                nums[i] = Math.floor(nums[i] / 10);
+            }
+        }
+    }
+    console.log(sumOfDigits)
+    let res = sumOfElements - sumOfDigits
+    return res
+};
+differenceOfSum([1, 2, 3 ,11]);
+
+// let sumOfDigits = 0;
+// let num = 515
+// let rem;
+// while (num != 0) {
+//     rem = num % 10;
+//     sumOfDigits += rem;
+//     num = Math.floor(num / 10);
+// }
+// console.log(sumOfDigits);
