d168f641f4c0ebf7234ec69aeca4f1c11880337f
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-25 21:46:27 +0530

    leet code update

---
+++ leet-code/leet-code-1460.js
@@ -0,0 +1,10 @@
+// 1460. Make Two Arrays Equal by Reversing Subarrays
+// Easy
+// 919
+// 130
+// Companies
+// You are given two integer arrays of equal length target and arr. In one step, you can select any non-empty subarray of arr and reverse it. You are allowed to make any number of steps.
+
+// Return true if you can make arr equal to target or false otherwise.
+
+ 
