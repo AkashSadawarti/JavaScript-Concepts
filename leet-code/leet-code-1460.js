ddc46cdbc43a2c8fa4c0eeb77bb13ea29d349739
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-25 21:48:58 +0530

    leetcode update

--- leet-code/leet-code-1460.js
+++ leet-code/leet-code-1460.js
@@ -24,5 +24,33 @@ let res1 = target.sort()
 let res2 = arr.sort()
 console.log(res1.toString() === res2.toString())
 
+// 2nd method
+// let newTarget;
+// let temp;
+// for (let i = 0; i < target.length; i++) {
+//     for (let j = 0; j < target.length; j++) {
+//         if (target[i] > target[j]) {
+//             temp = target[i];
+//             target[i] = target[j]
+//             target[j] = temp;
+//         }
+//     }
+// }
+// newTarget = target;
+
+// let newArr;
+// let temp2;
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length; j++) {
+//         if (arr[i] > arr[j]) {
+//             temp2 = arr[i];
+//             arr[i] = arr[j]
+//             arr[j] = temp2;
+//         }
+//     }
+// }
+// newArr = arr; 
+// return newTarget.toString() === newArr.toString()
+
 
 
