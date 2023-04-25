f599e41f199646d755f6032d3b476f8c0e4a229c
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-25 21:47:24 +0530

    leet code update

--- leet-code/leet-code-1460.js
+++ leet-code/leet-code-1460.js
@@ -8,3 +8,15 @@
 // Return true if you can make arr equal to target or false otherwise.
 
  
+
+// Example 1:
+
+// Input: target = [1,2,3,4], arr = [2,4,1,3]
+// Output: true
+// Explanation: You can follow the next steps to convert arr to target:
+// 1- Reverse subarray [2,4,1], arr becomes [1,4,2,3]
+// 2- Reverse subarray [4,2], arr becomes [1,2,4,3]
+// 3- Reverse subarray [4,3], arr becomes [1,2,3,4]
+// There are multiple ways to convert arr to target, this is not the only way to do so.
+
+
