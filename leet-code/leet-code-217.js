cca11775946b3fa904199add719273bbf5dc6c82
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-29 11:49:06 +0530

    leet code  update

--- leet-code/leet-code-217.js
+++ leet-code/leet-code-217.js
@@ -2,6 +2,16 @@
 
 // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
- 
+ // Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: true
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+
+
+
 
 
