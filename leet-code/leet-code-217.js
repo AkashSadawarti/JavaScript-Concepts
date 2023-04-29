9f10aadc6a2c2c397cdbb549c81f66a7ff76d390
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-29 11:49:28 +0530

    leet code update

--- leet-code/leet-code-217.js
+++ leet-code/leet-code-217.js
@@ -12,6 +12,14 @@
 // Output: false
 
 
-
+let nums = [1,2,3,4]
+for (let i = 0; i < nums.length; i++) {
+    for (let j = i+1; j < nums.length; j++) {
+        if (nums[i] == nums[j]) {
+            console.log(true); 
+        }
+    }
+}
+console.log(false); 
 
 
