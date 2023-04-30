a1e6b70a204738ac06ec490342ddd8f6068162ed
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-04-30 23:46:27 +0530

    leet code 2648

---
+++ leet-code/leet-code-2648.js
@@ -0,0 +1,12 @@
+// 2648. Generate Fibonacci Sequence
+// Easy
+// 22
+// 3
+// Companies
+// Write a generator function that returns a generator object which yields the fibonacci sequence.
+
+// The fibonacci sequence is defined by the relation Xn = Xn-1 + Xn-2.
+
+// The first few numbers of the series are 0, 1, 1, 2, 3, 5, 8, 13.
+
+ 
