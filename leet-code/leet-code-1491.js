448a9694b17242b6d77dd98d5d5382c57b8425ac
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-05-02 13:54:06 +0530

    leet code update

--- leet-code/leet-code-1491.js
+++ leet-code/leet-code-1491.js
@@ -11,5 +11,24 @@
 // Explanation: Minimum salary and maximum salary are 1000 and 4000 respectively.
 // Average salary excluding minimum and maximum salary is (2000+3000) / 2 = 2500
 
-
+let salary = [25000, 48000, 57000, 86000, 33000, 10000, 42000, 3000, 54000, 29000, 79000, 40000]
+let min ;
+for (let i = 0; i < salary.length; i++) {
+    for (let j = i+1; j < salary.length; j++) {
+        if(salary[i] > salary[j]){
+            min = salary[i];
+            salary[i] = salary[j];
+            salary[j] = min
+        }
+   }
+}
+console.log(salary);
+let count = 0;
+let sum = 0;
+for (let i = 1; i <= salary.length - 2; i++) {
+    sum += salary[i];
+    count++
+}
+let res = sum / count;
+console.log(res)
 
