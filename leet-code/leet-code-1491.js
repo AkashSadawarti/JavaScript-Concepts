70a55cb69cd1e3d7552590541bbdac335a448790
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-05-02 13:53:46 +0530

    leet code update

--- leet-code/leet-code-1491.js
+++ leet-code/leet-code-1491.js
@@ -4,5 +4,12 @@
 
 // Return the average salary of employees excluding the minimum and maximum salary. Answers within 10-5 of the actual answer will be accepted.
 
+// Example 1:
+
+// Input: salary = [4000,3000,1000,2000]
+// Output: 2500.00000
+// Explanation: Minimum salary and maximum salary are 1000 and 4000 respectively.
+// Average salary excluding minimum and maximum salary is (2000+3000) / 2 = 2500
+
 
 
