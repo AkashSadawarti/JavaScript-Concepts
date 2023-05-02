eb53ef82e46f0efd73da4f6a484c0de6f9c420bb
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-05-02 13:53:12 +0530

    leet code

---
+++ leet-code/leet-code-1491.js
@@ -0,0 +1,8 @@
+// 1491. Average Salary Excluding the Minimum and Maximum Salary
+
+// You are given an array of unique integers salary where salary[i] is the salary of the ith employee.
+
+// Return the average salary of employees excluding the minimum and maximum salary. Answers within 10-5 of the actual answer will be accepted.
+
+
+
