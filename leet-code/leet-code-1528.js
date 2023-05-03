31485720f48761b04bc4059356588a939b160c55
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-05-03 17:33:52 +0530

    leet-code update

--- leet-code/leet-code-1528.js
+++ leet-code/leet-code-1528.js
@@ -3,3 +3,13 @@
 // You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
 
 // Return the shuffled string.
+
+ 
+
+// Example 1:
+
+
+// Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+// Output: "leetcode"
+// Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+
