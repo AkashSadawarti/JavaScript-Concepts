7c790dfa9c8a7b2a36f57357daa000f509406c4d
Akash Sadawarti <sadawarti.akash@gmail.com>
2023-07-31 12:23:59 +0530

    input output command update

--- JSProgram3ConsoleInputOutput/JSProgram3ConsoleInputOutput.js
+++ JSProgram3ConsoleInputOutput/JSProgram3ConsoleInputOutput.js
@@ -4,7 +4,21 @@
 console.log('Hello Akash, Welcome to JavaScript Learning!')
 
 //For User input on Console window
-let showMessage = prompt('Hi.. Enter your Name')
-console.log(showMessage)
-console.log(`Welcome to JavaScript Learning ${showMessage}.`)
+// let showMessage = prompt('Hi.. Enter your Name')
+// console.log(showMessage)
+// console.log(`Welcome to JavaScript Learning ${showMessage}.`)
+
+
+// For user input number
+// let number = prompt("Enter any number")
+// let number2 = parseInt(number);
+// document.write(number2)  // It gives output on screen
+// console.log(number2)  // it gives output in console window
+
+
+// use of alert with document.write
+alert("Value of A is required")
+const a = prompt("Enter the value of A here");
+document.write(a)
+console.log(a);
 
